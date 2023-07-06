9972f875ceefb695cca55bb1a408958918695a09
Anirudh Sriram <anirudh.sriram96@gmail.com>
2023-07-06 15:33:27 -0400

    Create boilerplate search bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
+import { useState } from "react";
+
+const Header = ({ onInputChange, onSearchClick }) => {
+  return (
+  <div className="searchBar">
+    <h2>Pixabay Search Bar</h2>
+    <input 
+      onChange={e => onInputChange(e.target.value)}
+      placeholder={"Enter search here..."}
+    />
+    <button onClick={() => onSearchClick()}>
+      Search
+    </button>
+  </div>
+  );
+}
 
 function App() {
+  console.log("Rendering App");
+
+  const [ searchText, setSearchText ] = useState("");
+  const search = () => {
+    
+  }
+
+  console.log("Search text: ", searchText);
+  
   return (
     <div className="App">
-      Hello
+      <Header
+        onInputChange={setSearchText}
+        onSearchClick={search}
+      />
     </div>
   );
 }
