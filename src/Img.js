7bccf6d4d322765bc9d4d8fb64704274c8965174
Anirudh Sriram <anirudh.sriram96@gmail.com>
2023-07-06 18:42:55 -0400

    Finish basic functionality: - Search - Click - Carousel

--- src/Img.js
+++ src/Img.js
@@ -1,8 +1,21 @@
 import { useLoaderData } from "react-router-dom";
 
 const Img = () => {
-    const { imgDetails } = useLoaderData();
-    console.log("Image data:");
+    const data = useLoaderData().data.hits[0];
+    console.log("Image data in img", data);
+
+    return(
+        <div>
+            <img
+                src={data.largeImageURL}
+                alt="Full image"
+            />
+            <p>Original uploader: {data.user}</p>
+            <img 
+                src={data.userImageURL}
+            />
+        </div>
+    );
 }
 
 export { Img };
